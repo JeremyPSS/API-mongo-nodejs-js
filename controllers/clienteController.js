de4c8a5dc2e04f47dab60ab4b9318f9b1f56bc70
Jeremy Prieto <jeremy2002_prieto@hotmail.com>
2024-02-23 22:30:52 -0500

    README changed

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -13,8 +13,6 @@ function verifyToken(req, res, next) {
         if (err) {
             return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
         }
-
-        // If everything is good, save to request for use in other routes
         req.userId = decoded.id;
         next();
     });
